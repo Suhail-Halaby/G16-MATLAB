177851163e6f4a867940d8a189428eda2770188a
BelgiqueEnVelo <guanhao.du23@imperial.ac.uk>
2025-05-21 17:57:29 +0100

    Add files via upload

---
+++ Robust_2_varlist.m
@@ -0,0 +1,19 @@
+% CONSTANT
+g = 9.81;
+
+% Uncertain friction
+mu = 0.14; % Varies: 0.11 - 0.17
+ST_gram = 130;
+
+% Masses (kg)
+m_prop = 6 / 1000;
+m_motor = 76 / 1000;
+m_esc = 42 / 1000;
+m_des = 200 / 1000;
+m_cw = 0 / 1000;
+
+% Lengths (m)
+l_T = 3.5 / 100;
+l_g = 8.5 / 100;
+l_c = 5.5 / 100;
+h = 21.04 / 100;
